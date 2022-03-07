264b054172464753bc3773ab3de6df495c37f96e
Suhas <nanisuhas3@gmail.com>
2022-03-07 16:49:38 +0530

    Add files via upload

---
+++ file_extension.py
@@ -0,0 +1,8 @@
+#Second Task
+
+#User input for the file name
+name = str(input("Enter the file name: "))
+
+#Concatinating the file name with the python file extension
+print("The file name is: " + name + ".py")
+
