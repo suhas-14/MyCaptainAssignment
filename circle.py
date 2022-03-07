7e674301079c947ba5d7bac186033b076ae4b779
Suhas <nanisuhas3@gmail.com>
2022-03-07 16:41:25 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,15 @@
+
+#First Task
+
+#From the math library we are importing the pi value
+from math import pi
+
+#Taking the input from the user
+r= float(input("Input the radius of the circle: "))
+
+#Calculating and printing out the area of a circle with radius given by the user.
+print("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2)) 
+
+
+
+
